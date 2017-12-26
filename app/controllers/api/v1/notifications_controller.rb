3d18f9e48c053d28f395d906d2f9d5083d0e1b73
webstardeveloper <johnmartin3387@gmail.com>
2017-12-25 21:46:58 -0600

    notification

---
+++ app/controllers/api/v1/notifications_controller.rb
@@ -0,0 +1,12 @@
+module Api
+  module V1
+    class NotificationsController < ApplicationController
+      before_action :find_notification
+
+      
+
+    end
+  end
+end
+
+
---
+++ app/controllers/api/v1/projects_controller.rb
@@ -0,0 +1,17 @@
+module Api
+  module V1
+    class ProjectsController < ApplicationController
+
+      before_action :authenticate_request, only: [:create, :update, :get_draft_project, :fund_project, :launch, :report_project]
+      before_action :find_project, only: [:show, :launch, :destroy, :show, :fund_project, :report_project, :get_project_backers]
+
+      def index
+        @projects = Project.all.where(aasm_state: "funding")
+        render json: @projects, each_serializer: LiteProjectSerializer, status: :ok
+      end
+
+      
+
+    end
+  end
+end
